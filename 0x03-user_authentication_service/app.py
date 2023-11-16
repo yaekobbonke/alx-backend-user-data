d329c68a2a6189604af00fbdec473db39be98944
yaekobbonke <yaekobbundure.gmail.com>
2023-11-16 21:03:12 +0300

    app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -88,6 +88,7 @@ def get_reset_password_token():
     except ValueError:
         abort(403)
 
+
 @app.route('/reset_password', methods=['PUT'])
 def update_password():
     """Update password end-point
