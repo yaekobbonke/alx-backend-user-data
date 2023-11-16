812a4ab562e01154e18d516c774f7ed2f00b6c96
yaekobbonke <yaekobbundure.gmail.com>
2023-11-16 20:04:57 +0300

    app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -32,19 +32,75 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/session', methods=['POST'], strict_slash=False)
-def login():
-    """Log in"""
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def session() -> str:
+    """ Credentials validation """
 
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if AUTH.valid_login():
-        session_id = create_session()
-        session['session_id'] = session_id
-
+    res = AUTH.valid_login(email, password)
+    if res:
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
     else:
-        abort()
+        abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """ logout
+    """
+    session_cookie = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_cookie)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
+
+
+@app.route('/profile', methods=['GET'])
+def profile():
+    """User profile
+    """
+    cookie = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(cookie)
+    if user:
+        return jsonify({"email": user.email})
+    else:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """Get reset password token
+    """
+    email = request.form.get('email')
+    try:
+        new_token = get_reset_password_token(email)
+        if new_token:
+            return {"email": email, "reset_token": new_token}
+        else:
+            abort(403)
+    except ValueError:
+        abort(403)
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    """Update password end-point
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    try:
+        update_password('reset_token')
+        return {"email": email, "message": "Password updated"}, 200
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
