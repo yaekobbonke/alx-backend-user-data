697aa87b90b70b61ce9a34689492fa6f851b97a0
yaekobbonke <yaekobbundure.gmail.com>
2023-11-17 05:41:41 +0300

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,52 +1,42 @@
 #!/usr/bin/env python3
-
-"""DB module
-"""
+""" data base """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
+from user import Base, User
+from typing import TypeVar
 from sqlalchemy.exc import InvalidRequestError
-
-from user import Base
-from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
-    """DB class
-    """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+    def __init__(self):
+        """ initializing database """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
+        """session """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """ returns a User object
-        """
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """ adds user to the database"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs) -> User:
-        """ Find user by specified criteria
-        """
+    def find_user_by(self, **kargs) -> TypeVar('User'):
+        """ find user from database"""
         try:
-            user = self._session.query(User).filter_by(**kwargs).first()
+            user = self._session.query(User).filter_by(**kargs).first()
         except TypeError:
             raise InvalidRequestError
         if user is None:
@@ -54,8 +44,7 @@ class DB:
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """update user
-        """
+        """ updates user in databse"""
         user = self.find_user_by(id=user_id)
         for x, y in kwargs.items():
             if hasattr(user, x):
